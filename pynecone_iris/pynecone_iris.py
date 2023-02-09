a446dd55f319024fa3aaa9aa31cad3c85d13c57a
sanjivch <sanjiv.ch09@gmail.com>
2023-02-10 00:15:59 +0530

    Added predict functionality

--- pynecone_iris/pynecone_iris.py
+++ pynecone_iris/pynecone_iris.py
@@ -9,27 +9,31 @@ docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 # Loading model to compare the results
-model_lr = pickle.load(open('model_lr.pkl','rb'))
-
-x = np.array([4.9, 3.0, 1.4, 0.2])
-x=x.reshape(1,-1)
-y_pred = model_lr.predict(x)
-print(y_pred)
 
 class State(pc.State):
     """The app state."""
 
-    pass
-import random
-class VarNumberState(pc.State):
-    number: int
+    sepal_length: float = 0.0
+    sepal_width: float = 0.0
+    petal_length: float = 0.0
+    petal_width: float = 0.0
 
-    def update(self):
-        self.number = random.randint(0, 100)
+    species: str = ""
+    def get_prediction(self):
+        """Get the prediction"""
+        model_lr = pickle.load(open('model_lr.pkl','rb'))
+
+        x = np.array([self.sepal_length, self.sepal_width, self.petal_length, self.petal_width])
+        x = x.reshape(1,-1)
+        self.species = model_lr.predict(x)[0]
+        
 
 
-class NumberInputState(pc.State):
-    number: float
+
+
+
+# class NumberInputState(pc.State):
+#     number: float
 
 def index():
     return pc.center(
@@ -37,16 +41,22 @@ def index():
             pc.heading("Iris Flower Classification", font_size="24px"),
             pc.form_control(
                 pc.form_label("Sepal Length"),
-                pc.number_input(on_change=NumberInputState.set_number,),
-                pc.form_label("Sepal Length"),
-                pc.number_input(on_change=NumberInputState.set_number,),
-                pc.form_label("Sepal Length"),
-                pc.number_input(on_change=NumberInputState.set_number,),
-                pc.form_label("Sepal Length"),
-                pc.number_input(on_change=NumberInputState.set_number,),
+                pc.number_input(placeholder="Enter sepal length..",on_blur=State.set_sepal_length),
+                pc.form_label("Sepal Width"),
+                pc.number_input(placeholder="Enter sepal width..",on_blur=State.set_sepal_width),
+                pc.form_label("Petal Length"),
+                pc.number_input(placeholder="Enter petal length..",on_blur=State.set_petal_length),
+                pc.form_label("Petal Width"),
+                pc.number_input(placeholder="Enter petal width..",on_blur=State.set_petal_width),
                 is_required=True,
             ),
-            pc.heading(y_pred[0], font_size="20px"),
+            pc.button(
+                "Predict",
+                on_click=[State.get_prediction],
+                width="100%",
+            ),
+            pc.divider(),
+            pc.heading(State.species, font_size="20px"),
         ),
         
         padding_top="10%",
